27a82c271077bbf3f8d6169ab0ad4ad7b5677dd4
Tanzim <tanzim67@gmail.com>
2022-04-16 02:07:28 +0600

    3-Progress Indicator bar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/inertia-vue3'
-
+import { InertiaProgress } from '@inertiajs/progress'
 createInertiaApp({
   resolve: name => require(`./Pages/${name}`),
   setup({ el, App, props, plugin }) {
@@ -8,4 +8,20 @@ createInertiaApp({
       .use(plugin)
       .mount(el)
   },
+})
+InertiaProgress.init()
+
+InertiaProgress.init({
+  // The delay after which the progress bar will
+  // appear during navigation, in milliseconds.
+  delay: 250,
+
+  // The color of the progress bar.
+  color: 'red',
+
+  // Whether to include the default NProgress styles.
+  includeCSS: true,
+
+  // Whether the NProgress spinner will be shown.
+  showSpinner: false,
 })
