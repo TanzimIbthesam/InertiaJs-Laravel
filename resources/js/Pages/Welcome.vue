240018f32b6f05c0dd3ea3316f10e5e1919d1ed0
Tanzim <tanzim67@gmail.com>
2022-04-16 01:14:48 +0600

    1-Installation,props and route

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,22 @@
+<template>
+    Welcomee-{{name}}
+    <div v-for="framework in frameworks" :key="framework.id">
+          {{framework}}
+    </div>
+</template>
+
+
+
+ <script setup>
+const props=defineProps({
+    name:{
+        type:String,
+        required:true
+    },
+    frameworks:{
+        type:Array
+    }
+})
+
+
+</script>
---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//Routing in Inertia
+Route::get('/', function () {
+    // return view('welcome');
+    return inertia('Welcome',[
+        'name'=>'Tanzim',
+         'frameworks'=>[
+             'Laravel','Inertia','Vue'
+         ]
+    ]);
+});
